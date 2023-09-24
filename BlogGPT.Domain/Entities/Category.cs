85ae7efcae9b88b927874cf5bc07967c27631685
vtvy <votrieuvy1661@gmail.com>
2023-09-24 22:05:14 +0700

    Add category entity

---
+++ BlogGPT.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogGPT.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Slug { get; set; } = default!;
+        public int? ParentCategoryId { get; set; }
+        public ICollection<Category>? ChildrenCategories { get; set; }
+
+    }
+}
--- BlogGPT.UI/Program.cs
+++ BlogGPT.UI/Program.cs
@@ -10,13 +10,53 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
-//app.UseStaticFiles();
+app.UseStaticFiles();
 
 //app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseStatusCodePages(handleError =>
+{
+    handleError.Run(async context =>
+    {
+        await context.Response.WriteAsync(
+            @$"<html lang=""en"">
+                <head>
+                    <meta charset=""UTF-8"" />
+                    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
+                    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"" />
+                    <title>Bootstrap 5 404 Error Page</title>
+                    <link href=""/lib/bootstrap/dist/css/bootstrap.min.css"" rel=""stylesheet"">
+                </head>
+                <body>
+                    <div class=""d-flex align-items-center justify-content-center vh-100"">
+                        <div class=""text-center"">
+                            <h1 class=""display-1 fw-bold"">404</h1>
+                            <p class=""fs-3""> <span class=""text-danger"">Opps!</span> Page not found.</p>
+                            <p class=""lead"">
+                                The page you’re looking for does not exist.
+                              </p>
+                            <a href=""/"" class=""btn btn-primary"">Go Home</a>
+                        </div>
+                    </div>
+                </body>
+               </html>"
+            );
+    });
+});
 
 app.Run();
