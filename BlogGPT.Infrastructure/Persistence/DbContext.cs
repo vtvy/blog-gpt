697afe3167ff1e471982034d46d6098a1d52a2c0
Vo Trieu Vy <vyvt@gsoft.com.vn>
2023-09-26 17:51:01 +0700

    Add dbcontext and change identity installation

---
+++ BlogGPT.Infrastructure/Persistence/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using BlogGPT.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogGPT.Infrastructure.Persistence
+{
+    public class DbContext : IdentityDbContext<User>
+    {
+        public DbContext() { }
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+    }
+}
